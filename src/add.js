2f9a94c8695f0fe570fa73222da0bbe8ed0df851
Justin Fanton <justin.fanton@gmail.com>
2024-10-04 15:38:36 +0200

    lab done

--- src/add.js
+++ src/add.js
@@ -2,6 +2,8 @@ function add (numOne, numTwo) {
     if (numOne == undefined || numTwo == undefined) {
         return undefined;
     }
-
+    else if(numOne === " " || numTwo === " "){
+        return undefined;
+    }
     return numOne + numTwo;
 }
--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,10 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it("Should return undefined if any of the two aruments is not a number", () => {
+        expect(add(" "," ")).toEqual(undefined);
+      });
     });
   });
   
